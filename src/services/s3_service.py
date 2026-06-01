4707a8ab267350eeb0f3b3edf0d90452bf4706f5
Metehan Muhammed Torun <metehan_575@hotmail.com>
2026-06-01 17:24:48 +0300

    fix: refactor S3Service to use lazy loading and update CI/CD workflow

--- src/services/s3_service.py
+++ src/services/s3_service.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Optional
 import boto3
 from botocore.exceptions import ClientError
@@ -6,24 +7,28 @@ from ..models import Coupon
 
 class S3Service:
     def __init__(self):
+        self.endpoint_url = os.getenv("S3_ENDPOINT_URL", "http://localstack:4566")
         self.s3 = boto3.resource(
             "s3",
-            endpoint_url="http://localstack:4566",
+            endpoint_url=self.endpoint_url,
             aws_access_key_id="test",
             aws_secret_access_key="test",
             region_name="us-east-1"
         )
         self.bucket_name = "coupon-vault"
         self.bucket = self.s3.Bucket(self.bucket_name)
-        self._ensure_bucket_exists()
+        self.bucket_exists = False  
 
     def _ensure_bucket_exists(self):
-        try:
-            self.bucket.create()
-        except ClientError:
-            pass
+        if not self.bucket_exists:
+            try:
+                self.bucket.create()
+                self.bucket_exists = True
+            except ClientError:
+                self.bucket_exists = True 
 
     def save_coupon(self, coupon: Coupon) -> bool:
+        self._ensure_bucket_exists() 
         try:
             self.bucket.put_object(
                 Key=f"{coupon.code}.json",
@@ -34,6 +39,7 @@ class S3Service:
             return False
 
     def get_coupon(self, code: str) -> Optional[Coupon]:
+        self._ensure_bucket_exists()
         try:
             obj = self.s3.Object(self.bucket_name, f"{code}.json")
             data = json.loads(obj.get()["Body"].read().decode("utf-8"))
@@ -42,6 +48,7 @@ class S3Service:
             return None
 
     def list_coupons(self) -> list[Coupon]:
+        self._ensure_bucket_exists()
         coupons = []
         try:
             for obj in self.bucket.objects.all():
@@ -55,6 +62,7 @@ class S3Service:
         return coupons
 
     def delete_coupon(self, code: str) -> bool:
+        self._ensure_bucket_exists()
         try:
             self.s3.Object(self.bucket_name, f"{code}.json").delete()
             return True
